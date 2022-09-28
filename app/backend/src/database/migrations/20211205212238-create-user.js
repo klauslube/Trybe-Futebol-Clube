d8d9f6843b301c31e19f244e17119e63e2696a6f
klauslube <klauslube93@gmail.com>
2022-09-27 23:12:36 -0300

    feat: criada migration de users

---
+++ app/backend/src/database/migrations/20211205212238-create-user.js
@@ -0,0 +1,36 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      username: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      role: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: true,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      }
+    },
+      {
+        timestamps: false,
+      },
+    )
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('users');
+  },
+};
