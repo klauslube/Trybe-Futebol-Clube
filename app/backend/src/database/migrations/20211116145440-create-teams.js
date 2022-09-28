36f3c7f82510b2ce430460e080f42ff6e6b8e2e2
klauslube <klauslube93@gmail.com>
2022-09-27 23:30:26 -0300

    feat: criado migration teams

---
+++ app/backend/src/database/migrations/20211116145440-create-teams.js
@@ -0,0 +1,24 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('teams', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      team_name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: true,
+      },
+    },
+      {
+        timestamps: false,
+      },
+    )
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('teams');
+  },
+};
