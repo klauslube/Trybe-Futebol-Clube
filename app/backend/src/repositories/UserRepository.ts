ab978a763600272d9142594c0c3f4b0e52c69a14
klauslube <klauslube93@gmail.com>
2022-09-28 23:46:59 -0300

    feat: criado camada UserRepository

---
+++ app/backend/src/repositories/UserRepository.ts
@@ -0,0 +1,10 @@
+import ILogin from '../interfaces/ILogin';
+import User from '../database/models/User';
+
+export default class UserRepository {
+  userModel = User;
+  async findOne(userData:ILogin) {
+    const user = await this.userModel.findOne({ where: { email: userData.email } });
+    return user;
+  }
+}
